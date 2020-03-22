888459af8ba8afe0735c7499ad702b8c6d543afc
Aku Pyykkönen <akupyy@gmail.com>
2020-03-22 11:02:34 +0200

    fixed text size on mobile

--- public/testi.js
+++ public/testi.js
@@ -62,19 +62,19 @@ function draw() {
   if (floor(((ajastin)-timer)/60) < 3) {
     textAlign(CENTER, CENTER);
     fill(255);
-    textSize(vh / 2);
+    textSize(min(vh, vw) / 2);
     text(3, vw/4, vh/2);
   }
   if (floor(((ajastin)-timer)/60) < 2) {
     textAlign(CENTER, CENTER);
     fill(255);
-    textSize(vh / 2);
+    textSize(min(vh, vw) / 2);
     text(2, vw/2, vh/2);
   }
   if (floor(((ajastin)-timer)/60) < 1) {
     textAlign(CENTER, CENTER);
     fill(255);
-    textSize(vh / 2);
+    textSize(min(vh, vw) / 2);
     text(1, vw/4*3, vh/2);
   }
   if (timer > (ajastin)) {
